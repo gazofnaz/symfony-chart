aa356817345ac5224d90175c4a06f94865ac66f6
Gareth Arnott <gareth.arnott@informa.com>
2015-06-20 13:35:05 +0100

    next step for buildQuery and next test

--- src/AppBundle/Tests/Utils/QueryTest.php
+++ src/AppBundle/Tests/Utils/QueryTest.php
@@ -4,8 +4,33 @@ namespace AppBundle\Tests\Utils;
 
 use AppBundle\Utils\Query;
 
+/**
+ * More of a suite of functional tests than unit tests
+ *
+ * BDD is not practical because this code is never rendered
+ *
+ * Class QueryTest
+ * @package AppBundle\Tests\Utils
+ */
 class QueryTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  \PHPUnit_Framework_MockObject_MockObject $container */
+    private $container;
+
+    /** @var  \PHPUnit_Framework_MockObject_MockObject $container */
+    private $logger;
+
+    /** @var  \PHPUnit_Framework_MockObject_MockObject $container */
+    private $user;
+
+    public function setUp(){
+        $this->container = $this->getMock( '\\Symfony\\Component\\DependencyInjection\\ContainerInterface' );
+        $this->logger = $this->getMockBuilder( '\\Monolog\\Logger' )
+                       ->disableOriginalConstructor()
+                       ->getMock();
+        $this->user = $this->getMock( '\\AppBundle\\Entity\User' );
+    }
+
     /**
      * Tests that the multidimensional query array has the expected keys
      *
@@ -19,25 +44,58 @@ class QueryTest extends \PHPUnit_Framework_TestCase
      */
     public function testBuildQueryKeysExist()
     {
-        $container = $this->getMock( '\\Symfony\\Component\\DependencyInjection\\ContainerInterface' );
-        $logger = $this->getMockBuilder( '\\Monolog\\Logger' )
-            ->disableOriginalConstructor()
-            ->getMock();
-        $user = $this->getMock( '\\AppBundle\\Entity\User' );
-        $user
+
+        $query = new Query( $this->container,  $this->logger );
+
+        $resultArray = $query->buildQuery( $this->user );
+
+        $this->assertArrayHasKey( 'query'   , $resultArray );
+        $this->assertInternalType( 'array'  , $resultArray['query'], 'the query subset is not an array' );
+
+        $this->assertArrayHasKey( 'api-key' , $resultArray['query'] );
+        $this->assertArrayHasKey( 'q'       , $resultArray['query'] );
+        $this->assertArrayHasKey( 'order-by', $resultArray['query'] );
+
+
+    }
+
+    /**
+     * After we've confirmed the keys exist, we check the values of the keys.
+     *
+     * We don't want to check against our stored parameters because we might accidentally
+     * change one resulting in an application failure.
+     *
+     * @todo this isn't actually testing anything, because everything is being mocked... need to get real data from the container
+     *
+     * @depends testBuildQueryKeysExist
+     */
+    public function testBuildQueryKeyValues()
+    {
+        $map = array(
+            array( 'guardian_api.key'     , 'q5kmfff8753v6q2kfq7m3rgu' ),
+            array( 'guardian_api.order_by', 'new' )
+        );
+
+        $this->container->expects( $this->any() )
+                  ->method( 'getParameter' )
+                  ->will( $this->returnValueMap( $map ) );
+
+        $this->user
             ->expects( $this->any() )
             ->method( 'getFirstName' )
             ->will($this->returnValue( 'Gareth' ));
 
-        $query = new Query( $container,  $logger );
+        $query = new Query( $this->container,  $this->logger );
 
-        $resultArray = $query->buildQuery( $user );
+        $resultArray = $query->buildQuery( $this->user );
 
-        $this->assertArrayHasKey( 'query'   , $resultArray );
-        $this->assertArrayHasKey( 'q'       , $resultArray['query'] );
-        $this->assertArrayHasKey( 'key'     , $resultArray['query'] );
-        $this->assertArrayHasKey( 'order-by', $resultArray['query'] );
+        $expectedSubset = array(
+            'api-key'       => $this->container->getParameter( 'guardian_api.key' ),
+            'q'             => $this->user->getFirstName(),
+            'order-by'      => $this->container->getParameter( 'guardian_api.order_by' )
+            );
 
+        $this->assertArraySubset( $resultArray['query'],  $expectedSubset );
 
     }
 }
--- src/AppBundle/Utils/Query.php
+++ src/AppBundle/Utils/Query.php
@@ -7,6 +7,18 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Query
 {
+    /**
+     * As per Symfony best practices, parameters which will almost never
+     * change should be class constants and not yml parameters
+     */
+    const API_KEY       = 'api-key';
+    const ORDER_BY      = 'order-by';
+    const SEARCH_QUERY  = 'q';
+    const START_DATE    = 'from-date';
+    const END_DATE      = 'to-date';
+    const PAGE_NUMBER   = 'page';
+    const PAGE_SIZE     = 'page-size';
+
     /** @var ContainerInterface $container */
     private $container;
 
@@ -21,10 +33,27 @@ class Query
         $this->logger = $logger;
     }
 
+    /**
+     * @param User $user
+     * @return array
+     */
     public function buildQuery( User $user ){
 
+        /** @throws \InvalidArgumentException */
+        $key = $this->container->getParameter( 'guardian_api.key' );
+
+        // @todo throw exception for empty name
+        $firstName = $user->getFirstName();
+
+        /** @throws \InvalidArgumentException */
+        $orderBy = $this->container->getParameter( 'guardian_api.order_by' );
+
         $query = array(
-            'query' => array()
+            'query' => array(
+                self::API_KEY       => $key,
+                self::SEARCH_QUERY  => $firstName,
+                self::ORDER_BY      => $orderBy
+            )
         );
 
         return $query;
