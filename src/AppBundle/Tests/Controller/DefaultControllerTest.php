05655a75a24878b13dc129f9f8818fc000dadcfc
Gareth Arnott <gareth.arnott@informa.com>
2016-02-13 13:09:43 +0000

    add nice message to index controller test

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,6 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request( 'GET', '/' );
 
         $this->assertEquals( 200, $client->getResponse()->getStatusCode() );
-        $this->assertTrue( $crawler->filter( 'html:contains("Occurrences")' )->count() > 0 );
+        $this->assertTrue( $crawler->filter( 'html:contains("Occurrences")' )->count() > 0, 'Could not find word Occurrences on index page' );
     }
 }
